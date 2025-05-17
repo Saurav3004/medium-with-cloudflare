08815191932f86df3df234d8d9f097ed86c44e75
Saurav3004 <130168711+Saurav3004@users.noreply.github.com>
2025-05-17 21:45:20 +0530

    added zod schema

---
+++ common/src/index.ts
@@ -0,0 +1,23 @@
+import z from 'zod'
+
+export const signupInput = z.object({
+    email: z.string().email(),
+    password: z.string().min(6),
+    name: z.string().optional()
+})
+
+export type SignupInput = z.infer<typeof signupInput>
+
+export const signinInput = z.object({
+    email: z.string().email(),
+    password: z.string().min(6),
+})
+
+export type SigninInput = z.infer<typeof signinInput>
+
+export const createBlogInput = z.object({
+    title: z.string().max(20),
+    content:z.string().max(200)
+})
+
+export type CreateBlogInput = z.infer<typeof createBlogInput>
